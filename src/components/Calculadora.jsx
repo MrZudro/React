811ecf5227d175a885126b570d85da177275c584
EivorKinKest <jmqo2015@gmail.com>
2025-04-07 06:01:47 -0500

    useState de Calculadora

--- src/components/Calculadora.jsx
+++ src/components/Calculadora.jsx
@@ -1,17 +1,23 @@
+import { useState } from 'react'
 import './estilos/Calculadora.css'
 
 function Calculadora() {
+    const [clicky, setClicky] = useState('')
+    const handleNumber = () =>  {
+        const numeroElegido = event.target.textContent
+        setClicky(clicky + numeroElegido)
+    }
 
     return (
         <div className='GeneralDiv'>
             <div className='containerCalculadora'>
             <div>
-                <input type="text" id='resultado' readOnly />
+                <input type="text" id='resultado' value={clicky} readOnly />
             </div>
             <div>
-                <button className='esnumero'>1</button>
-                <button className='esnumero'>2</button>
-                <button className='esnumero'>3</button>
+                <button className='esnumero' onClick={handleNumber}>1</button>
+                <button className='esnumero' onClick={handleNumber}>2</button>
+                <button className='esnumero' onClick={handleNumber}>3</button>
                 <button className='operador'>+</button>
             </div>
             <div>
